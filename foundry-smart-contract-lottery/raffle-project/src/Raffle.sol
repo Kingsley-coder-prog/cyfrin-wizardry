6fcad1910846c98012b064a09487b4a9d18adb66
Kingsley <kifeanyi216@gmail.com>
2025-08-26 15:03:17 +0100

    The CEI Method - Checks, Effect, Interactions

--- foundry-smart-contract-lottery/raffle-project/src/Raffle.sol
+++ foundry-smart-contract-lottery/raffle-project/src/Raffle.sol
@@ -118,17 +118,22 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 requestId,
         uint256[] calldata randomWords
     ) internal override {
+        // Checks
+
+        // Effect (Internal Contract State)
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
         s_players = new address payable[](0);
         s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(s_recentWinner);
+
+        // Interactions (External Contract Interactions)
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
         }
-        emit WinnerPicked(s_recentWinner);
     }
 
     // Getter Functions
