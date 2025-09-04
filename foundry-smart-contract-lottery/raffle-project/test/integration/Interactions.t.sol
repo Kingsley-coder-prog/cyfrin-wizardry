d2df458281c7e051addfa47ccd633bf4bfaba4b0
“Kingsley-coder-prog” <kifeanyi216@gmail.com>
2025-09-04 15:31:51 +0100

    Integration Test

---
+++ foundry-smart-contract-lottery/raffle-project/test/integration/Interactions.t.sol
@@ -0,0 +1,12 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.19;
+
+// unit tests
+// integration tests
+// forked tests
+// staging tests <- run tests on a mainnet or testnet
+
+// fuzzing
+// stateful fuzz
+// stateless fuzz
+// formal verification
