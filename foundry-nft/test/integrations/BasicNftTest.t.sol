4519dfde1e4893772a20f8a8765a0795a4b6aee5
Kingsley <kifeanyi216@gmail.com>
2025-09-17 22:25:04 +0100

    Minor Fixes II

--- foundry-nft/test/integrations/BasicNftTest.t.sol
+++ foundry-nft/test/integrations/BasicNftTest.t.sol
@@ -14,7 +14,7 @@ contract BasicNftTest is Test {
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public {
-        deployer = new DeployBasicNft();
+        deployer = new DeployBasicNft(); // create an instance of the deployer contract
         basicNft = deployer.run(); // deploy the contract
     }
 
