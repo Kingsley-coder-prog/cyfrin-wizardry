cd5a34fb34ab646c877691aea2c1525cbef81bc5
Kingsley <kifeanyi216@gmail.com>
2025-09-18 01:40:51 +0100

    Encoding Session

---
+++ foundry-nft/freestyle/Encoding.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract Encoding {
+    function combineString() public pure returns (string memory) {
+        return string(abi.encodePacked("Hi Mom ", "Miss you!"));
+    }
+
+    function encodeNumber() public pure returns (bytes memory) {
+        bytes memory number = abi.encode(1);
+        return number;
+    }
+
+    function encodeString() public pure returns (bytes memory) {
+        bytes memory someString = abi.encode("some string");
+        return someString;
+    }
+
+    function encodeStringPacked() public pure returns (bytes memory) {
+        bytes memory someString = abi.encodePacked("some string");
+        return someString;
+    }
+
+    function encodeStringBytes() public pure returns (bytes memory) {
+        bytes memory someString = bytes("some string");
+        return someString;
+    }
+
+    function decodeString() public pure returns (string memory) {
+        string memory someString = abi.decode(encodeString(), (string));
+        return someString;
+    }
+
+    function multiEncode() public pure returns (bytes memory) {
+        bytes memory someString = abi.encode("some string", "It's a bigger");
+        return someString;
+    }
+
+    function multiDecode() public pure returns (string memory, string memory) {
+        (string memory someString, string memory someOtherString) = abi.decode(
+            multiEncode(),
+            (string, string)
+        );
+        return (someString, someOtherString);
+    }
+
+    function multiEncodePacked() public pure returns (bytes memory) {
+        bytes memory someString = abi.encodePacked(
+            "some string",
+            "It's bigger"
+        );
+        return someString;
+    }
+
+    function multiDecodePacked() public pure returns (string memory) {
+        string memory someString = abi.decode(multiEncodePacked(), (string));
+        return someString;
+    }
+
+    function multiStringCastPacked() public pure returns (string memory) {
+        string memory someString = string(multiEncodePacked());
+        return someString;
+    }
+}
