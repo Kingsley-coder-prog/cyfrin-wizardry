bd4b031cbeba124644fca7832cfc4c1209dcc18f
Kingsley <kifeanyi216@gmail.com>
2025-10-20 16:58:58 +0100

    Write testRedeemStrightAway

--- ccip-rebase-token/src/RebaseToken.sol
+++ ccip-rebase-token/src/RebaseToken.sol
@@ -15,9 +15,9 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
     
-    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private constant PRECISION_FACTOR = 1e27;
     bytes32 public constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
-    uint256 private s_interestRate = 5e10;
+    uint256 private s_interestRate = (5 * PRECISION_FACTOR) / 1e8; // 10^-8 == 1/10^8
     mapping (address => uint256) private s_userInterestRate;
     mapping (address => uint256) private s_userLastUpdatedTimestamp;
 
@@ -86,7 +86,7 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     function balanceOf(address _user) public view override returns (uint256) {
         // get the current principle balance of the user (the number of token that have actually been minted to the user)
         // multiply the principle balance by the interest that has accumulated in the time since the balance
-        return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user);
+        return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
     }
 
     /**
--- ccip-rebase-token/test/RebaseToken.t.sol
+++ ccip-rebase-token/test/RebaseToken.t.sol
@@ -24,14 +24,39 @@ contract RebaseTokenTest is Test {
     }
 
     function testDepositLinear(uint256 amount) public {
-        vm.assume(amount > 1e5);
-        amount = bound(amount, 1e5, type(uint256).max);
+        amount = bound(amount, 1e5, type(uint96).max);
         // 1. deposit
         vm.startPrank(user);
         vm.deal(user, amount);
+        vault.deposit{value: amount}();
         // 2. check our rebase token balance
+        uint256 startBalance = rebaseToken.balanceOf(user);
+        console.log("startBalance", startBalance);
+        assertEq(startBalance, amount);
         // 3. warp the time and check the balance again
+        vm.warp(block.timestamp + 1 hours);
+        uint256 middleBalance = rebaseToken.balanceOf(user);
+        assertGt(middleBalance, startBalance);
         // 4. warp the time again by the same amount and check the balance again
+        vm.warp(block.timestamp + 1 hours);
+        uint256 endBalance = rebaseToken.balanceOf(user);
+        assertGt(endBalance, middleBalance);
+
+        assertApproxEqAbs(endBalance - middleBalance, middleBalance - startBalance, 1);
+
+        vm.stopPrank();
+    }
+
+    function testRedeemStraightAway(uint256 amount) public {
+        amount = bound(amount, 1e5, type(uint96).max);
+        // 1. deposit
+        vm.startPrank(user);
+        vm.deal(user, amount);
+        vault.deposit{value: amount}();
+        assertEq(rebaseToken.balanceOf(user), amount);
+        // 2. redeem
+        vault.redeem(type(uint256).max);
+        assertEq(rebaseToken.balanceOf(user), 0);
         vm.stopPrank();
     }
 }
