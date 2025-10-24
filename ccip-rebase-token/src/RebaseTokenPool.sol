aaf558d3e6d642c8727c447d92ec35574b8aa739
Kingsley <kifeanyi216@gmail.com>
2025-10-24 15:15:33 +0100

    Finish Pool Contract

--- ccip-rebase-token/src/RebaseTokenPool.sol
+++ ccip-rebase-token/src/RebaseTokenPool.sol
@@ -9,12 +9,12 @@ import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8
 import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
 
 contract RebaseToken is TokenPool {
-    constructor(address _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, 18, _allowlist, _rmnProxy, _router) {}
+    constructor(IERC20 _token, address[] memory _allowlist, address _rmnProxy, address _router) TokenPool(_token, 18, _allowlist, _rmnProxy, _router) {}
 
     function lockOrBurn(Pool.LockOrBurnInV1 calldata lockOrBurnIn) external returns (Pool.LockOrBurnOutV1 memory lockOrBurnOut) {
         _validateLockOrBurn(lockOrBurnIn);
-        address originalSender = abi.decode(lockOrBurnIn.originalSender, (address));
-        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(originalSender);
+        // address originalSender = abi.decode(lockOrBurnIn.originalSender, (address));
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(lockOrBurnIn.originalSender);
         IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
         lockOrBurnOut = Pool.LockOrBurnOutV1({
             destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
@@ -24,11 +24,28 @@ contract RebaseToken is TokenPool {
 
     function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn) external returns (Pool.ReleaseOrMintOutV1 memory) {
         _validateReleaseOrMint(releaseOrMintIn);
-        uint256 userInterestRate = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
-        IRebaseToken(address(i_token)).mint(releaseOrMintIn.receiver, releaseOrMintIn.amount, userInterestRate);
+        address receiver = releaseOrMintIn.receiver;
+        (uint256 userInterestRate) = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+        // Mint rebasing tokens to the receiver on the destination chain
+        // This will also mint any interest that has accrued since the last time the user's balance was updated.
+        IRebaseToken(address(i_token)).mint(receiver, releaseOrMintIn.amount, userInterestRate);
 
-        return Pool.ReleaseOrMintOutv1({
+        return Pool.ReleaseOrMintOutV1({
             destinationAmount: releaseOrMintIn.amount
         });
     }
+
+    // function releaseOrMint(Pool.ReleaseOrMintInV1 calldata releaseOrMintIn)
+    //     external
+    //     returns (Pool.ReleaseOrMintOutV1 memory)
+    // {
+    //     _validateReleaseOrMint(releaseOrMintIn);
+    //     address receiver = releaseOrMintIn.receiver;
+    //     (uint256 userInterestRate) = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+    //     // Mint rebasing tokens to the receiver on the destination chain
+    //     // This will also mint any interest that has accrued since the last time the user's balance was updated.
+    //     IRebaseToken(address(i_token)).mint(receiver, releaseOrMintIn.amount, userInterestRate);
+
+    //     return Pool.ReleaseOrMintOutV1({destinationAmount: releaseOrMintIn.amount});
+    // }
 }
