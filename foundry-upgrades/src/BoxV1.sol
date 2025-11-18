101059be1214b5addab3f258b4d9ae098ee7c8da
Kingsley <kifeanyi216@gmail.com>
2025-11-18 20:14:30 +0100

    Deploy Upgradeable SC

--- foundry-upgrades/src/BoxV1.sol
+++ foundry-upgrades/src/BoxV1.sol
@@ -2,16 +2,16 @@
 
 pragma solidity ^0.8.18;
 
-import {UUPSUpgradeable} "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     // @custom:0z-upgrades-unsafe-allow constructor
     constructor() {
-        _disableInitializer();
+        _disableInitializers();
     }
 
     function initialize() public initializer
--- foundry-upgrades/src/BoxV2.sol
+++ foundry-upgrades/src/BoxV2.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {UUPSUpgradeable} "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 
 contract BoxV2 is  UUPSUpgradeable {
