d4a43ae456db7c8fa6a84c796cd6fc8535fddcd4
Kingsley <kifeanyi216@gmail.com>
2026-01-13 12:27:17 +0100

    Input Fiels AI

---
+++ full-stack-web3/ts-tsender-ui/ts-tsender-ui/src/components/AirdropForm.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import InputField from "@/components/ui/InputField"
+import {useState} from "react"
+
+export default function AirdropForm() {
+    const [tokenAddress, setTokenAddress] = useState("")
+    return (
+        <div>
+            <InputField label="Token Address" placeholder="0x" value={tokenAddress} onChange={e => setTokenAddress(e.target.value)} />
+        </div>
+    )
+}
