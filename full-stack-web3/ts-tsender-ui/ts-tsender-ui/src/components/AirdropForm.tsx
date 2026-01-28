524956bb7d952d51cd5dc2e126d9810b4eb03f83
Kingsley <kifeanyi216@gmail.com>
2026-01-28 16:03:40 +0100

    Airdrop Form I

--- full-stack-web3/ts-tsender-ui/ts-tsender-ui/src/components/AirdropForm.tsx
+++ full-stack-web3/ts-tsender-ui/ts-tsender-ui/src/components/AirdropForm.tsx
@@ -2,14 +2,37 @@
 
 import InputField from "@/components/ui/InputField"
 import {useState} from "react"
+import { chainsToTSender, tsenderAbi, erc20Abi} from "@/constants"
+import {useChainId, useConfig, useConnection} from 'wagmi'
+import { readContract } from "@wagmi/core"
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipients, setRecipients] = useState("")
     const [amounts, setAmounts] = useState("")
+    const chainId = useChainId()
+    const config = useConfig()
+    const account = useConnection()
+
+
+    async function getApprovedAmount(tSenderAddress: string | null): Promise<number> {
+        if (!tSenderAddress) {
+            alert("No address found, please use a supported chain")
+            return 0
+        }
+        const response = await readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            functionName: "allowance",
+            args: [account.address, tSenderAddress as `0x${string}`]
+        })
+        return response as number
+    }
 
     async function handleSubmit() {
-        console.log("Hi from submit")
+        const tSenderAddress = chainsToTSender[chainId]["tsender"]
+        const approvedAmount = await getApprovedAmount(tSenderAddress)
+        console.log(approvedAmount)
     }
 
     return (
@@ -20,7 +43,7 @@ export default function AirdropForm() {
 
             <InputField label="Amount" placeholder="100, 200, 300...." value={amounts} onChange={e => setAmounts(e.target.value)} large={true} />
 
-            <button onClick={handleSubmit}>
+            <button onClick={handleSubmit} className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-sm transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed">
                 Send tokens
             </button>
         </div>
